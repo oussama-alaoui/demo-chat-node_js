db450e92cf634970ca942b69bdbc265b9c5c5975
Oussama Alaoui chrifi <oalaoui-@e2r13p2.1337.ma>
2022-04-23 00:49:35 +0000

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+// server.listen(3000, () => {
+//     console.log('listening on *:3000');
+// });
+module.exports = router;
